8b059718e79a5f54b7c742aee877d03b587e7b02
Your Name <you@example.com>
2023-02-06 17:07:35 +0530

    hj

--- src/app/chat/app2/app2.component.ts
+++ src/app/chat/app2/app2.component.ts
@@ -4,48 +4,44 @@ import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-app2',
   templateUrl: './app2.component.html',
-  styleUrls: ['./app2.component.scss']
+  styleUrls: ['./app2.component.scss'],
 })
 export class App2Component {
-  constructor(private chatapi : ChatService){}
-  chats:any=[]
-  ngOnInit(){
-this.receivemessage()
-}
-myForm:any = new FormGroup({
-  Username: new FormControl('dipen'),
-    Chat: new FormControl('')
-  });
-check=false
-ngDoCheck(){
-  // if(this.check==true){
-    // this.receivemessage()
-
-  // }
-  }
-  ngOnChanges(){
+  constructor(private chatapi: ChatService) {}
+  chats: any = [];
+  ngOnInit() {
     setInterval(() => {
-      this.receivemessage()
+      this.receivemessage();
     }, 1000);
   }
-  receivemessage(){
-this.chatapi.getmessage().subscribe((res:any)=>{
-  this.chats=res.data
-  // console.log(this.chats);
-})
+  myForm: any = new FormGroup({
+    Username: new FormControl('dipen'),
+    Chat: new FormControl(''),
+  });
+  check = false;
+  ngDoCheck() {
+    // if(this.check==true){
+    // this.receivemessage()
+    // }
   }
-  val:any=null
-  send(){
+  ngOnChanges() {}
+  receivemessage() {
+    this.chatapi.getmessage().subscribe((res: any) => {
+      this.chats = res.data;
+      // console.log(this.chats);
+    });
+  }
+  val: any = null;
+  send() {
     //   const data:any={
     //     Username:"azad",
     // Chat:vale
     //   }
-      this.chatapi.msgsend(this.myForm.value).subscribe((res:any)=>{
-        // this.chats=res.data
-        // console.log(res);
-        this.myForm.get('Chat').reset();
-        this.ngOnInit()
-      })
-    }
-
+    this.chatapi.msgsend(this.myForm.value).subscribe((res: any) => {
+      // this.chats=res.data
+      // console.log(res);
+      this.myForm.get('Chat').reset();
+      this.ngOnInit();
+    });
+  }
 }
